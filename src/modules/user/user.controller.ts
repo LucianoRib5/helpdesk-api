df394529542aec8ff3e590a8739d90bddb8c8a61
Luciano Ribeiro <luciano.eng.soft@outlook.com>
2025-07-25 16:55:26 -0300

    feat: cria controlador de usuário com método para criação de usuários

---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UserService } from './user.service';
+
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async createUser(@Body() body: CreateUserDto): Promise<void> {
+    await this.userService.createUser(body);
+  }
+}
